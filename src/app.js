89ab461e2d6f195dfef9574daee9aebe4c44249d
Jacob-Wendt <jacob.wendt@yhaoo.com>
2019-10-10 10:24:22 -0500

    cleaned up code in app

--- src/app.js
+++ src/app.js
@@ -4,16 +4,15 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
-// const chalk = require('chalk');
 
 // Esoteric Resources
 const errorHandler = require( './middleware/error.js');
 const notFound = require( './middleware/404.js' );
 
 // Models
-
-// const Products = require('./models/products.js');
-// const products = new Products();
+// TODO: Pull these in (or create them)!
+const Products = require('./models/products.js');
+const products = new Products();
 
 const Categories = require('./models/categories.js');
 const categories = new Categories();
@@ -28,23 +27,20 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-// Routes (categories)
+// Routes
 app.get('/api/v1/categories', getCategories);
 app.post('/api/v1/categories', postCategories);
-
 app.get('/api/v1/categories/:id', getCategory);
 app.put('/api/v1/categories/:id', putCategories);
 app.delete('/api/v1/categories/:id', deleteCategories);
 
-// // Routes (products)
-// app.get('/api/v1/products', getProducts);
-// app.post('/api/v1/products', postProducts);
+app.get('/api/v1/products', getProducts);
+app.post('/api/v1/products', postProducts);
+app.get('/api/v1/products/:id', getProduct);
+app.put('/api/v1/products/:id', putProducts);
+app.delete('/api/v1/products/:id', deleteProducts);
 
-// app.get('/api/v1/products/:id', getProduct);
-// app.put('/api/v1/products/:id', putProducts);
-// app.delete('/api/v1/products/:id', deleteProducts);
-
-// Middleware
+// Catchalls
 app.use(notFound);
 app.use(errorHandler);
 
@@ -62,7 +58,6 @@ function getCategories(request,response,next) {
     .catch( next );
 }
 
-//CRUD ROUTES
 function getCategory(request,response,next) {
   // expects an array with the one matching record from the model
   categories.get(request.params.id)
@@ -72,10 +67,11 @@ function getCategory(request,response,next) {
 
 function postCategories(request,response,next) {
   // expects the record that was just added to the database
-  categories.create(request.body)
+  categories.post(request.body)
     .then( result => response.status(200).json(result[0]) )
     .catch( next );
 }
+
 
 function putCategories(request,response,next) {
   // expects the record that was just updated in the database
@@ -92,47 +88,47 @@ function deleteCategories(request,response,next) {
 }
 
 
-// function getProducts(request,response,next) {
-//   // expects an array of objects back
-//   products.get()
-//     .then( data => {
-//       const output = {
-//         count: data.length,
-//         results: data,
-//       };
-//       response.status(200).json(output);
-//     })
-//     .catch( next );
-// }
+function getProducts(request,response,next) {
+  // expects an array of objects back
+  products.get()
+    .then( data => {
+      const output = {
+        count: data.length,
+        results: data,
+      };
+      response.status(200).json(output);
+    })
+    .catch( next );
+}
 
-// function getProduct(request,response,next) {
-//   // expects an array with one object in it
-//   products.get(request.params.id)
-//     .then( result => response.status(200).json(result[0]) )
-//     .catch( next );
-// }
+function getProduct(request,response,next) {
+  // expects an array with one object in it
+  products.get(request.params.id)
+    .then( result => response.status(200).json(result[0]) )
+    .catch( next );
+}
 
-// function postProducts(request,response,next) {
-//   // expects the record that was just added to the database
-//   products.post(request.body)
-//     .then( result => response.status(200).json(result) )
-//     .catch( next );
-// }
+function postProducts(request,response,next) {
+  // expects the record that was just added to the database
+  products.post(request.body)
+    .then( result => response.status(200).json(result) )
+    .catch( next );
+}
 
 
-// function putProducts(request,response,next) {
-//   // expects the record that was just updated in the database
-//   products.put(request.params.id, request.body)
-//     .then( result => response.status(200).json(result) )
-//     .catch( next );
-// }
+function putProducts(request,response,next) {
+  // expects the record that was just updated in the database
+  products.put(request.params.id, request.body)
+    .then( result => response.status(200).json(result) )
+    .catch( next );
+}
 
-// function deleteProducts(request,response,next) {
-//   // Expects no return value (the resource should be gone)
-//   products.delete(request.params.id)
-//     .then( result => response.status(200).json(result) )
-//     .catch( next );
-// }
+function deleteProducts(request,response,next) {
+  // Expects no return value (the resource should be gone)
+  products.delete(request.params.id)
+    .then( result => response.status(200).json(result) )
+    .catch( next );
+}
 
 
 
